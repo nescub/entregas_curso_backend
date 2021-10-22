cecda7dbd1dbb933f8f92ca908e96fd5e8f0e8db
Nestor Cubilla <nescub@hotmail.com>
2021-10-22 14:13:32 -0300

    primer commit entrega desafio motores de plantillas usando pug

---
+++ 005-motores_de_plantillas_pug/src/class/contenedor.js
@@ -0,0 +1,76 @@
+/* Ejemplo de object:                                                                                                                                             
+    {
+        title: (nombre del producto),
+        price: (precio),
+        thumbnail: (url de la foto del producto)
+        id: (id numerico asignado por contendor)
+    }                                                                                                                                               
+*/
+
+class Contenedor {
+    constructor() {
+        this.idCount = 0;
+        this.objects = [];
+    }
+    
+    save(newObject){
+        this.idCount = this.idCount + 1
+        newObject.id = this.idCount
+
+        this.objects.push(newObject)
+        
+        return newObject
+    }
+
+    indexById(objectId) {
+        for (let i = 0; i < this.objects.length; i++) {
+            if ( (this.objects[i]).id == objectId ) {
+               return i
+            }
+         }
+        
+        return null
+    }
+
+    getById(objectId) {
+        const index = this.indexById(objectId)
+        
+        if(index == null) {
+            return null
+        }
+        
+        return this.objects[index]
+    }
+
+    getAll() {
+        return this.objects
+    }
+
+    deleteById(objectId) {
+        const index = this.indexById(objectId)
+
+        if(index != null) {
+            const eliminated = this.objects.splice(index, 1)
+            return eliminated[0]
+        }
+
+        return null
+    }
+
+    update(id, newObject){
+        const currentObject = this.getById(id)
+
+        if( currentObject == null) {
+            return null
+        }
+
+        for (const property in newObject) {
+            currentObject[property] = newObject[property]          
+        }
+                
+        return currentObject
+    }
+
+}
+
+module.exports = Contenedor
---
+++ 005-motores_de_plantillas_pug/src/main.js
@@ -0,0 +1,37 @@
+//Levanta un server en el puerto 8080, con un motor de plantillas pug
+
+const express = require('express');
+
+const Contenedor = require('./class/contenedor.js')   //clase Contenedor
+
+const contenedorProductos = new Contenedor()
+const app = express();
+
+app.use(express.urlencoded({ extended: true }))
+
+app.set('views', './views');     // especifica el directorio de vistas
+app.set('view engine', 'pug');   // registra el motor de plantillas
+
+// Configuro rutas:
+
+app.get('/', (req, res) => {
+    res.render('formulario');
+});
+
+app.get('/productos', (req, res) => {
+    res.render('tabla', { productos: contenedorProductos.getAll() } );
+});
+
+app.post('/productos', (req, res) => {
+    contenedorProductos.save(req.body);
+    console.log(contenedorProductos);
+    res.redirect('/');
+});
+
+// Levanto el server:
+
+const PORT = 8080;
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+ });
+server.on("error", error => console.log(`Error en servidor ${error}`));
